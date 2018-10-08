dd2858fc25efcbb3b3bcb94d86399260d37d9519
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-10-08 19:53:21 +0800

    update v1.1
    update executor, change interface to func.
    update readme

--- context.go
+++ context.go
@@ -6,21 +6,21 @@ import (
 )
 
 const (
-	ctxKeyPreparer = "preparer"
+	ctxKeyPreparable = "_preparable"
 )
 
-type Preparer interface {
+type Preparable interface {
 	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
 }
 
-func WithPreparer(parent context.Context, p Preparer) context.Context {
-	return context.WithValue(parent, ctxKeyPreparer, p)
+func WithPreparable(parent context.Context, p Preparable) context.Context {
+	return context.WithValue(parent, ctxKeyPreparable, p)
 }
 
-func prepare(ctx context.Context) Preparer {
-	v := ctx.Value(ctxKeyPreparer)
+func prepare(ctx context.Context) Preparable {
+	v := ctx.Value(ctxKeyPreparable)
 	if v == nil {
 		return nil
 	}
-	return v.(Preparer)
+	return v.(Preparable)
 }
--- executor.go
+++ executor.go
@@ -7,54 +7,56 @@ import (
 	"fmt"
 )
 
-type Executable interface {
-	Exec(ctx context.Context, stmt *sql.Stmt) (result sql.Result, err error)
-}
+type ExecuteFunc func(ctx context.Context, stmt *sql.Stmt, row interface{}) (result sql.Result, err error)
 
-func Execute(ctx context.Context, query string, executables ...Executable) (affected int64, err error) {
+func Execute(ctx context.Context, query string, fn ExecuteFunc, rows ...interface{}) (affected int64, err error) {
 	if ctx == nil {
-		err = errors.New("dalc-> execute failed, context is empty")
+		err = errors.New("execute failed, context is empty")
 		return
 	}
 	if query == "" {
-		err = errors.New("dalc-> execute failed, query is empty")
+		err = errors.New("execute failed, query is empty")
 		return
 	}
-	if executables == nil || len(executables) == 0 {
-		err = errors.New("dalc-> execute failed, executables are empty")
+	if fn == nil {
+		err = errors.New("execute failed, execute function is empty")
+		return
+	}
+	if rows == nil || len(rows) == 0 {
+		err = errors.New("execute failed, rows are empty")
 		return
 	}
 	stmt, prepareErr := prepare(ctx).PrepareContext(ctx, query)
 	if prepareErr != nil {
-		err = fmt.Errorf("dalc-> execute failed, prepared statement failed. reason: %v", prepareErr)
+		err = fmt.Errorf("execute failed, prepared statement failed. reason: %v", prepareErr)
 		return
 	}
 	defer func() {
 		stmtCloseErr := stmt.Close()
 		if stmtCloseErr != nil {
-			err = fmt.Errorf("dalc-> execute failed, close prepare statement failed. reason: %v", stmtCloseErr)
+			err = fmt.Errorf("execute failed, close prepare statement failed. reason: %v", stmtCloseErr)
 			return
 		}
 	}()
-	for _, executable := range executables {
-		result, execErr := executable.Exec(ctx, stmt)
+	for _, row := range rows {
+		result, execErr := fn(ctx, stmt, row)
 		if execErr != nil {
 			err = execErr
 			return
 		}
 		affectedRows, affectedErr := result.RowsAffected()
 		if affectedErr != nil {
-			err = fmt.Errorf("dalc-> execute failed, get result's affected failed. reason: %v, executable: %v", affectedErr, executable)
+			err = fmt.Errorf("execute failed, get result's affected failed. reason: %v, row: %v", affectedErr, row)
 			return
 		}
 		if affectedRows == 0 {
-			err = fmt.Errorf("dalc-> execute failed, affected nothing, executable: %v", executables)
+			err = fmt.Errorf("execute failed, affected nothing, row: %v", row)
 			return
 		}
 		affected = affected + affectedRows
 	}
 	if hasLog() {
-		logf("dalc-> execute success, affected: %d, sql: %s", affected, query)
+		logf("execute success, affected: %d, sql: %s", affected, query)
 	}
 	return
 }
